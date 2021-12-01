616ca403d29292a4ceb626cb8508b84fb528e029
israel-cs <israel.info@gmail.com>
2021-12-01 07:57:41 -0300

    Criando um VETOR em Java.

---
+++ Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		double[] vetor = new double[n];
+		for (int i=0;i<n;i++) {
+			vetor[i] = sc.nextDouble();			
+		}
+		
+		System.out.println();
+		double soma = 0;
+		for (int i=0;i<n;i++) {
+			soma += vetor[i];			
+		}
+		double resultado = soma/n;
+		System.out.printf("A média: %.2f%n", resultado);
+		sc.close();
+	}
+
+}
